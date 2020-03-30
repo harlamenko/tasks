260023152d939e03e91312826257b94d5d66e34f
Artem Harlamenko <4.harlamenko@gmail.com>
2020-03-30 20:01:06 +0300

    feature: route built task

---
+++ route_built/route_built.php
@@ -0,0 +1,132 @@
+<?php
+
+$write_result = require '../utils/write_result.php';
+
+function parse_connections($file) {
+    preg_match("/^(?'nodes_count'\d+)\s+(?'connections_count'\d+)$/", trim(fgets($file)), $matches);
+    $connections = [];
+
+    for ($i=0; $i < $matches['connections_count']; $i++) {
+        preg_match("/^(?'from_node'\d+)\s+(?'to_node'\d+)\s+(?'time'\d+)$/", trim(fgets($file)), $connections[]);
+    }
+
+    $reversed_connections = [];
+
+    foreach ($connections as $i => $connection) {
+        $connections[$i]['used'] = false;
+        $reversed_connections[] = [
+            'from_node' => $connection['to_node'],
+            'to_node' => $connection['from_node'],
+            'time' => $connection['time'],
+            'used' => false
+        ];
+    }
+    
+    return array_merge($connections, $reversed_connections);
+}
+
+function parse_requests($file) {
+    preg_match("/^(?'value'\d+)$/", trim(fgets($file)), $requests_count);
+    $requests = [];
+
+    for ($i=0; $i < $requests_count['value']; $i++) {
+        preg_match("/^(?'from_node'\d+)\s+(?'to_node'\d+)\s+(?'request_type'-1|\?|\d+)$/", trim(fgets($file)), $requests[]);
+    }
+    
+    return $requests;
+}
+
+function find_connection($req, $connections, $used_id = null) {
+    foreach ($connections as $id => $connection) {
+        if (
+            ($connection['from_node'] !== $req['from_node'] || $connection['to_node'] !== $req['to_node']) &&
+            ($connection['from_node'] !== $req['to_node'] || $connection['to_node'] !== $req['from_node'])
+        ) { continue; };
+        if ($used_id !== null && $used_id === $id) { continue; }
+        return $id;
+    }
+
+    return null;
+}
+
+function count_time($from, $to, $connections) {
+    $temp_connections = $connections;
+
+    foreach ($temp_connections as $i => $path) {
+        if (
+            $path['from_node'] !== $from ||
+            $path['used'] ||
+            $path['time'] === null
+        ) {
+            continue;
+        } else if ($path['to_node'] === $to) {
+            return $path['time'];
+        } else {
+            $temp_connections[$i]['used'] = true;
+            $back_connection_id = find_connection($path, $connections, $i);
+            $temp_connections[$back_connection_id]['used'] = true;
+            $time = count_time($path['to_node'], $to, $temp_connections);
+
+            if ($time !== -1) {
+                return $path['time'] + $time;
+            }
+        }
+    }
+
+    return -1;
+}
+
+function handle_request($req, $connections) {
+    switch ($req['request_type']) {
+        case '-1':
+            $connection_id = find_connection($req, $connections);
+            if ($connection_id !== null) {
+                $back_connection_id = find_connection($req, $connections, $connection_id);
+                $connections[$connection_id]['time'] = null;
+                $connections[$back_connection_id]['time'] = null;
+            }
+            break;
+        case '?':
+            $time = count_time($req['from_node'], $req['to_node'], $connections);
+            var_dump($time);
+            break;
+        default:
+            $connection_id = find_connection($req, $connections);
+            if ($connection_id === null) {
+                $back_connection_id = find_connection($req, $connections, $connection_id);
+                $connections[] = [
+                    'from_node' => $req['from_node'],
+                    'to_node' => $req['to_node'],
+                    'time' => $req['time'],
+                    'used' => false
+                ];
+                $connections[] = [
+                    'from_node' => $req['to_node'],
+                    'to_node' => $req['from_node'],
+                    'time' => $req['time'],
+                    'used' => false
+                ];
+            } else {
+                $connections[$connection_id]['time'] = $req['request_type'];
+            }
+            break;
+    }
+    return $connections;
+}
+
+function main() {
+    global $write_result;
+
+    $file = fopen('./input.txt','r') or die("не удалось открыть файл с входными данными");
+    $connections = parse_connections($file);
+    $requests = parse_requests($file);
+    $results = [];
+
+    foreach ($requests as $req) {
+        $connections = handle_request($req, $connections);
+    }
+
+    fclose($file);
+}
+
+main();
